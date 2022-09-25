1c5f2338e0b4940d4dc6310185bf81cdd117970e
justadamhopkins <adamhopkins87@gmail.com>
2022-09-25 23:08:46 +0100

    refactor: server and app js

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,35 @@
+import autoLoad from '@fastify/autoload'
 import fastify, {FastifyInstance} from 'fastify'
+import path from "path";
+import cors from '@fastify/cors'
+
+export const allowedCorsDomains: (string | RegExp)[] = [
+    /test\.com$/,
+    /localhost:[0-9]{4}$/,
+    /local\.lick\.com:[0-9]{4}$/,
+];
+
+const HTTP_OK = 200
 
 export const build = (opts = {}): FastifyInstance => {
     const app = fastify(opts);
+
+    app.register(cors, {
+        origin: false,
+        methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+        optionsSuccessStatus: HTTP_OK
+    })
+
+    app.register(autoLoad, {
+        dir: path.join(__dirname, 'routes'),
+        ignorePattern: /.*(test).ts/
+    })
+
+    app.register(autoLoad, {
+        dir: path.join(__dirname, 'services'),
+        ignorePattern: /.*(test).ts/
+    })
+
 
     return app;
 };
--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,15 @@ import fastify from "fastify";
 
 const DEFAULT_PORT = 5000
 
-const start = async () => {
+const startServer = async () => {
     try {
         const app = build({logger: true});
+        console.log('fired')
         await app.listen({port: DEFAULT_PORT})
     } catch (err) {
-        fastify.log.error(err)
+        fastify().log.error(err)
         process.exit(1)
     }
 }
 
-start()
+startServer()
