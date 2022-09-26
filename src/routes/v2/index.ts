cdd04d297682871c832a8a4a6e2f0da4e888d236
justadamhopkins <adamhopkins87@gmail.com>
2022-09-26 22:34:04 +0100

    feat: configured routes with versioning

--- src/routes/v2/index.ts
+++ src/routes/v2/index.ts
@@ -1,10 +1,11 @@
 import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
 
-const routes = (fastify: FastifyInstance, ops = {}) => {
-    
+const routes = (fastify: FastifyInstance, ops, done) => {
     fastify.get("/", async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
-        return reply.send({hello: "world"});
+        return reply.send({hello: "world v2"});
     });
+
+    done()
 }
 
 export default routes
