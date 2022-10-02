c35b8c035eec9a3f0c83abda6da9d7f584abf2f4
justadamhopkins <adamhopkins87@gmail.com>
2022-10-02 22:36:04 +0100

    chore: remove log

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,7 @@ const DEFAULT_PORT = 5000
 
 const startServer = async () => {
     try {
-        const app = build({logger: true});
-        console.log('fired')
+        const app = await build({logger: true});
         await app.listen({port: DEFAULT_PORT})
     } catch (err) {
         fastify().log.error(err)
