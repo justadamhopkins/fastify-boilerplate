3a0ae83eca94918ba80a339a3be7186cac4944d4
justadamhopkins <adamhopkins87@gmail.com>
2022-10-02 22:34:30 +0100

    chore: added fastify types file

---
+++ src/types/fastify.d.ts
@@ -0,0 +1,11 @@
+import Fastify from 'fastify'
+
+declare module 'fastify' {
+    interface FastifyInstance extends Fastify {
+        config: { // this should be same as the confKey in options
+            // specify your typing here
+            PORT: string
+            BUILD_ENV: string
+        };
+    }
+}
