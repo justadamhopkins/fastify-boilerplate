319faa3c3abfcf9f09c725278ceb7157ff971a73
justadamhopkins <adamhopkins87@gmail.com>
2022-10-30 21:23:26 +0000

    feat: added  type to v2 routes

--- src/routes/api/v2/index.ts
+++ src/routes/api/v2/index.ts
@@ -1,11 +1,28 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { API } from '@server/types';
+
+const schema = {
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        hello: { type: 'string' },
+      },
+    },
+  },
+};
 
 const v2Routes = (fastify: FastifyInstance, ops, done) => {
-  fastify.get('/', (request: FastifyRequest, reply: FastifyReply): void => {
-    reply.send({ hello: 'world v2' });
-  });
+  fastify.get('/', { schema }, getV2RouteCtrl);
 
   done();
+};
+
+const getV2RouteCtrl = (
+  request: FastifyRequest,
+  reply: API.IReplyWithPayload<{ hello: string }>
+): void => {
+  reply.send({ hello: 'world v2' });
 };
 
 export default v2Routes;
