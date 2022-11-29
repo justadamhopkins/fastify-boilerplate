7245246b5bb43843d552d3190a9d65e2c02f150f
justadamhopkins <adamhopkins87@gmail.com>
2022-11-29 00:02:59 +0000

    refactor: routes to not have api folder

--- src/routes/debug/autohooks.ts
+++ src/routes/debug/autohooks.ts
@@ -1,11 +1,11 @@
+import { notFound } from '@hapi/boom';
+import { APP_DEBUG_HEADER_KEY } from '@server/types/constants/api';
 import {
   FastifyInstance,
   FastifyReply,
   FastifyRequest,
   HookHandlerDoneFunction,
 } from 'fastify';
-import { notFound } from '@hapi/boom';
-import { APP_DEBUG_HEADER_KEY } from '@server/types/constants/api';
 
 const autoHooks = (fastify: FastifyInstance, ops, done) => {
   fastify.addHook(
@@ -19,7 +19,7 @@ const autoHooks = (fastify: FastifyInstance, ops, done) => {
         fastify.getHeaderValue(APP_DEBUG_HEADER_KEY, request) === 'true';
 
       if (!debugHeader) {
-        reply.send(notFound('Page not found'));
+        done(notFound('Page not found'));
       }
 
       done();
